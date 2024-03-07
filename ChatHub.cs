88dd152a60a239309b752118959a92fb0adf47f8
moinickcres <moinickcres@gmail.com>
2024-03-07 13:23:56 +0100

    Add files via upload

---
+++ ChatHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+
+namespace ChatRoom
+
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string room, string user, string message)
+        {
+            await Clients.Group(room).SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task AddToGroup(string room)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, room);
+
+            await Clients.Group(room).SendAsync("ShowWho", $"Alguien se conectó {Context.ConnectionId}");
+        }
+    }
+}
