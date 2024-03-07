5579c1b811a059b4c303f03554802362b1d16084
moinickcres <moinickcres@gmail.com>
2024-03-07 13:26:50 +0100

    Add files via upload

---
+++ Controllers/ChatController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChatRoom.Controllers
+{
+    public class ChatController : Controller
+    {
+        public static Dictionary<int, string> Rooms =
+            new Dictionary<int, string>()
+            {
+                { 1, "Cervezas" },
+                { 2, "Programación" },
+                { 3, "Moda" }
+            };
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Room(int room)
+        {
+            return View("Room", room);
+        }
+    }
+}
